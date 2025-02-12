a024a451cc03c630a0a93444074b9204c2825cba
Nygaaard <nygardandreas@hotmail.com>
2025-02-12 15:21:44 +0100

    Added todo item to JSON file

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using dotnetTodo.Models;
+using System.Text.Json;
 
 namespace dotnetTodo.Controllers;
 
+//Home Controller
 public class HomeController : Controller
 {
     public IActionResult Index()
@@ -12,7 +14,13 @@ public class HomeController : Controller
 
     public IActionResult Todos()
     {
-        return View();
+        //Läs in todos
+        string jsonStr = System.IO.File.ReadAllText("todos.json");
+        //Deserialisera JSON
+        var todos = JsonSerializer.Deserialize<List<TodoModel>>(jsonStr);
+
+        //Returnera vy
+        return View(todos);
     }
 
     public IActionResult AddTodo()
@@ -20,18 +28,32 @@ public class HomeController : Controller
         return View();
     }
 
-    [HttpPost]
+    [HttpPost] //Controller för post-anrop
     public IActionResult AddTodo(TodoModel model)
     {
         if (ModelState.IsValid)
         {
+            //Läs in todos
+            string jsonStr = System.IO.File.ReadAllText("todos.json");
+            //Deserialisera JSON
+            var todos = JsonSerializer.Deserialize<List<TodoModel>>(jsonStr);
 
-        }
-        else
-        {
+            //Lägg till todo
+            if (todos != null)
+            {
+                todos.Add(model);
 
+                //Serialisera JSON
+                jsonStr = JsonSerializer.Serialize(todos);
+                //Ändra i todos.json
+                System.IO.File.WriteAllText("todos.json", jsonStr);
+            }
+            //Rensa formulär
+            ModelState.Clear();
+
+            //Redirect till todos
+            return RedirectToAction("Todos", "Home");
         }
         return View();
     }
-
 }
