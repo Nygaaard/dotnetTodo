422784fc7950720e0f647abce2aeca6ca0ffbee9
Nygaaard <nygardandreas@hotmail.com>
2025-02-14 10:59:35 +0100

    Last changes

--- Models/TodosModel.cs
+++ Models/TodosModel.cs
@@ -8,11 +8,11 @@ public class TodoModel
 {
     //Properties
     [Required(ErrorMessage = "Ange en korrekt titel...")] // Ändrar error-meddelande
-    [MinLength(3)] //Minst 3 tecken
+    [MinLength(3, ErrorMessage = "Titeln måste vara minst 3 tecken...")] //Minst 3 tecken
     [Display(Name = "Titel:")] //Rendera Titel
     public required string Title { get; set; }
     [Required(ErrorMessage = "Ange en korrekt beskrivning...")] // Ändrar error-meddelande
-    [MinLength(5)] //Minst 5 tecken
+    [MinLength(5, ErrorMessage = "Beskrivningen måste vara minst 5 tecken...")] //Minst 5 tecken
     [Display(Name = "Beskrivning:")] //Rendera Beskrivning
     public required string Description { get; set; }
     [Required(ErrorMessage = "Ange korrekt status")] // Ändrar error-meddelande
